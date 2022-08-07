75a1c66fb77644ef6cab1d82400f665308112362
ssannino <sanninosperanza@gmail.com>
2022-08-07 19:38:06 +0200

    please

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,6 +1,10 @@
 import { useLocation } from "react-router-dom";
 import "url-search-params-polyfill";
 
-export default () => {
-  return new URLSearchParams(useLocation().search);
-};
+function Location() {
+  let location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  return searchParams;
+}
+
+export default Location;
